7b4af07d5f409907203cd6d888eea0142a56880a
Mathiyarasi-soundar <mathisoundar2004@gmail.com>
2024-10-21 17:18:16 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route element={<Home />} path='/mathi-tiles' />
+        <Route element={<Home />} path='/' />
         <Route element={<About />} path='/about' />
         <Route element={<Services />} path='/services' />
         <Route element={<ContactUs />} path='/contactus' />
--- src/Routes/Signup.jsx
+++ src/Routes/Signup.jsx
@@ -8,33 +8,35 @@ const Signup = () => {
 
   const [change, setChange] = useState("Login");
   return (
-    <div className='signup-container'>
-      <div className="signup-header">
-        <div className="signup-text">{change} </div>
-        <div className="signup-underline"></div>
-      </div>
-      <div className="signup-inputs">
-        {change === "Login" ? <div></div> : <div className="signup-input">
-          <i>{< FaRegUser />}</i>
-          <input type="text" placeholder='Name' />
-        </div>}
-
-        <div className="signup-input">
-          <i>{<MdOutlineEmail />}</i>
-          <input type="email" placeholder='E-mail' />
+    <section className='sign'>
+      <div className='signup-container'>
+        <div className="signup-header">
+          <div className="signup-text">{change} </div>
+          <div className="signup-underline"></div>
         </div>
-        <div className="signup-input">
-          <i>{<RiLockPasswordLine />}</i>
-          <input type="password" placeholder='Password' />
+        <div className="signup-inputs">
+          {change === "Login" ? <div></div> : <div className="signup-input">
+            <i>{< FaRegUser />}</i>
+            <input type="text" placeholder='Name' />
+          </div>}
+
+          <div className="signup-input">
+            <i>{<MdOutlineEmail />}</i>
+            <input type="email" placeholder='E-mail' />
+          </div>
+          <div className="signup-input">
+            <i>{<RiLockPasswordLine />}</i>
+            <input type="password" placeholder='Password' />
+          </div>
+        </div>
+        {change === "Sign Up" ? <div></div> : <div className="forgot-password">Forgot Password?<span>Click Here!</span></div>}
+
+        <div className="submit-container">
+          <div className={change === "Login" ? "submit gray" : "submit"} onClick={() => { setChange("Sign Up") }}> Sign Up</div>
+          <div className={change === "Sign up" ? "submit gray" : "submit"} onClick={() => { setChange("Login") }}>Login</div>
         </div>
       </div>
-      {change === "Sign Up" ? <div></div> : <div className="forgot-password">Forgot Password?<span>Click Here!</span></div>}
-
-      <div className="submit-container">
-        <div className={change === "Login" ? "submit gray" : "submit"} onClick={() => { setChange("Sign Up") }}> Sign Up</div>
-        <div className={change === "Sign up" ? "submit gray" : "submit"} onClick={() => { setChange("Login") }}>Login</div>
-      </div>
-    </div>
+    </section>
   )
 }
 
